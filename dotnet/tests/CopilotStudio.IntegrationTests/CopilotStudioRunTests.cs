33d09d263be4f98f50d3900af97aafdee4cd0357
westey <164392973+westey-m@users.noreply.github.com>
2025-07-11 11:58:37 +0100

    Set CopilotStudio integration tests to manual run only. (#166)

--- dotnet/tests/CopilotStudio.IntegrationTests/CopilotStudioRunTests.cs
+++ dotnet/tests/CopilotStudio.IntegrationTests/CopilotStudioRunTests.cs
@@ -7,9 +7,36 @@ namespace CopilotStudio.IntegrationTests;
 
 public class CopilotStudioRunTests() : RunTests<CopilotStudioFixture>(() => new())
 {
+    // Set to null to run the tests.
+    private const string ManualVerification = "For manual verification";
+
     [Fact(Skip = "Copilot Studio does not support thread history retrieval, so this test is not applicable.")]
     public override Task ThreadMaintainsHistoryAsync()
     {
         return Task.CompletedTask;
+    }
+
+    [Fact(Skip = ManualVerification)]
+    public override Task RunWithChatMessageReturnsExpectedResultAsync()
+    {
+        return base.RunWithChatMessageReturnsExpectedResultAsync();
+    }
+
+    [Fact(Skip = ManualVerification)]
+    public override Task RunWithChatMessagesReturnsExpectedResultAsync()
+    {
+        return base.RunWithChatMessagesReturnsExpectedResultAsync();
+    }
+
+    [Fact(Skip = ManualVerification)]
+    public override Task RunWithNoMessageDoesNotFailAsync()
+    {
+        return base.RunWithNoMessageDoesNotFailAsync();
+    }
+
+    [Fact(Skip = ManualVerification)]
+    public override Task RunWithStringReturnsExpectedResultAsync()
+    {
+        return base.RunWithStringReturnsExpectedResultAsync();
     }
 }

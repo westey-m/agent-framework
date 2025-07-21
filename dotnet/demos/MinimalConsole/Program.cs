a8fadbc49c0515939e22a33c9bec6e78226f174d
westey <164392973+westey-m@users.noreply.github.com>
2025-07-21 10:49:47 +0100

    Add a minimum console demo app (#198)
    * Add a minimum console demo app
    
    * Remove hardcoded values from sample.
    
    * Make console project .net 9.0 only.
    
    * Move minimum console demo to demo folder.
    
    * Fix framework issue with demo project.
    
    * Undo unintended commit.
    
    * Address PR comments
    
    * Fix build issue.
    
    * Address PR Comments
    
    * Rename Agent to AIAgent in new console app.

---
+++ dotnet/demos/MinimalConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.AI.Agents;
+
+[Description("Get the weather for a given location.")]
+static string GetWeather([Description("The location to get the weather for.")] string location)
+{
+    return $"The weather in {location} is cloudy with a high of 15°C.";
+}
+
+IChatClient chatClient = new AzureOpenAIClient(
+    new Uri(Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT")!),
+    new AzureCliCredential())
+     .GetChatClient("gpt-4o-mini")
+     .AsIChatClient();
+
+AIAgent agent = new ChatClientAgent(
+    chatClient,
+    instructions: "You are a helpful assistant, you can help the user with weather information.",
+    tools: [AIFunctionFactory.Create(GetWeather)]);
+
+Console.WriteLine(await agent.RunAsync("What's the weather in Amsterdam?"));

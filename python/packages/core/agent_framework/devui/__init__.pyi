2575e8cab0a29b03660fac5d39be66608ac53222
Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
2025-10-01 13:54:26 +0200

    Python: Packaging fixes (#1056)
    * small updates to setup
    
    * post1
    
    * reset version
    
    * updated package status

---
+++ python/packages/core/agent_framework/devui/__init__.pyi
@@ -0,0 +1,26 @@
+# Copyright (c) Microsoft. All rights reserved.
+from agent_framework_devui import (
+    AgentFrameworkRequest,
+    DevServer,
+    DiscoveryResponse,
+    EntityInfo,
+    OpenAIError,
+    OpenAIResponse,
+    ResponseStreamEvent,
+    __version__,
+    main,
+    serve,
+)
+
+__all__ = [
+    "AgentFrameworkRequest",
+    "DevServer",
+    "DiscoveryResponse",
+    "EntityInfo",
+    "OpenAIError",
+    "OpenAIResponse",
+    "ResponseStreamEvent",
+    "__version__",
+    "main",
+    "serve",
+]

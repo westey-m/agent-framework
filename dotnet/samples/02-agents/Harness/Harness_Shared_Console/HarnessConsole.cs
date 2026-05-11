7cf20adac500b5f27a7bf006d187f0e4ac9f04f5
westey <164392973+westey-m@users.noreply.github.com>
2026-05-11 19:01:45 +0000

    Fix formatting issues

--- dotnet/samples/02-agents/Harness/Harness_Shared_Console/HarnessConsole.cs
+++ dotnet/samples/02-agents/Harness/Harness_Shared_Console/HarnessConsole.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 
 using Harness.Shared.Console.Commands;
 using Harness.Shared.Console.Observers;
